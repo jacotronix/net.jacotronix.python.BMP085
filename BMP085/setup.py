66817d3c5607cbc097cafd8d626c827109cd154a
Jamie <jamie.d.jackson@gmail.com>
2012-12-27 18:37:36 +0000

    First commit

---
+++ BMP085/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+setup(name='BMP085',
+      version='0.0.1',
+      description='i2C BMP085 Pressure & Temperature Sensor Driver',
+      maintainer='Jamie Jackson',
+      maintainer_email='jamie@jacobean.net',
+      url='http://blog.jacobean.net',
+      py_modules=['BMP085'],
+      )
